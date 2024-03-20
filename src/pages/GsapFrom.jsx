4fc37f381162bce268473d17d110a827aa7701f4
skmajumder <skmajumder.cse@gmail.com>
2024-03-20 22:47:44 +0600

    GsapFrom()

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      rotate: 360,
+      duration: 3,
+      repeat: 2,
+      yoyo: true,
+      ease: "power1.inOut",
+    });
+  }, []);
 
   return (
     <main>
--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -2,8 +2,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 const GsapTo = () => {
-  // TODO: Implement the gsap.to() method
-
+  // * Implement the gsap.to() method
   useGSAP(() => {
     gsap.to("#blue-box", {
       x: 250,
